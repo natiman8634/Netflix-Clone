dee8bf33590aa152fdd162059d5455f7bc1c3ad7
natiman8634 <natiman863400@gmail.com>
2025-12-24 11:44:44 -0800

    header and footer

---
+++ netflix-clone/src/components/Header/header.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './header.css'
+import NetflixLogo from '../../assets/images/Netflix_Logo.png'
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+function header() {
+  return (
+    <div>
+        <div className="header_outer_container">
+      <div className="header_container">
+        <div className="header_left">
+          <ul>
+            <li>
+              <img src={NetflixLogo} alt="Netflix logo" width='100' />
+            
+            </li>
+            <li>Home</li>
+            <li>TVShows</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>MyList</li>
+            <li>Browse by Language</li>
+          </ul>
+        </div>
+        <div className="header_right">
+          <ul>
+            <li><SearchIcon/></li>
+            <li><NotificationsNoneIcon/></li>
+            <li><AccountBoxIcon/></li>
+            <li><ArrowDropDownIcon/></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default header
---
+++ netflix-clone/src/pages/Home/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../components/Header/header'
+import Footer from '../../components/Footer/footer'
+function home() {
+  return (
+    <>
+    <Header />
+    <Footer />
+    </>
+
+  )
+}
+
+export default home
