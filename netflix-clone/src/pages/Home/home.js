c3c39dbeb92c8159043ee60e03dbccd488445571
natiman8634 <natiman863400@gmail.com>
2025-12-25 10:57:33 -0800

    row

--- netflix-clone/src/pages/Home/home.js
+++ netflix-clone/src/pages/Home/home.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Header from '../../components/Header/header'
 import Footer from '../../components/Footer/footer'
 import Banner from '../../components/Banner/Banner'
+import RowList from '../../components/Rows/RowList/RowList'
 function home() {
   return (
     <>
     <Header />
     <Banner />
+    <RowList />
     <Footer />
     </>
 
